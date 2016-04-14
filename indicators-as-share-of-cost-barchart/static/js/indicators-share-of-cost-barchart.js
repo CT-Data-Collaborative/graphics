e0a1f7e25dcb18857ebaa6901f8790a5cc4ead36
Brendan Swiniarski <brendan.james.swiniarski@gmail.com>
2016-04-14 15:33:53 -0400

    Changed viz from stacked column chart to stacked bar chart - added tooltips, hover highlight/lowlights, titles, styling, etc.

--- indicators-as-share-of-cost-barchart/static/js/indicators-share-of-cost-barchart.js
+++ indicators-as-share-of-cost-barchart/static/js/indicators-share-of-cost-barchart.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     // variables
     var pymChild = null;
-    var aspect_ratio = (1 / 2); // H / W
+    var aspect_ratio = (2 / 3); // H / W
     var mobile_threshold = 500;
 
     // we need this in a few places
@@ -25,7 +25,7 @@ $(document).ready(function(){
     };
 
     var sluggify = function(text) {
-        if (d3.range(0,10).indexOf(text.slice(0)) !== 1) {
+        if (d3.range(0,10).indexOf(text.slice(0, 1)) !== 1) {
             text = "_" + text;
         }
         return text.toLowerCase().replace(/[^_0-9a-zA-Z]/ig, "_")
@@ -38,21 +38,6 @@ $(document).ready(function(){
     // var legendContainer = container.select("div#legend");
     /** END container vars **/
 
-    // tooltip functions
-    var townTip = d3.tip()
-        .attr("class", "barchart-tip")
-        .offset([-10, 0])
-        .html(function(d) {
-            return "Town Name"
-        });
-    var indicatorTip = d3.tip()
-        .attr("class", "barchart-tip")
-        .direction("e")
-        .offset([-10, 0])
-        .html(function(d) {
-            return "Indicator%"
-        });
-
     d3.csv("static/data/indicator-share-with-cogs.csv", function(data) {
         // type casting
         var DATA = data.map(function(o) {
@@ -62,7 +47,7 @@ $(document).ready(function(){
                 "Unemployment Rate" : parseFloat(o["Unemployment Rate (%)"]),
                 "Population Density" : parseFloat(o["Population Density (000s per square mile)"]),
                 "Private-Sector Wage Index" : parseFloat(o["Private-Sector Wage Index (%)"]),
-                "Town Maintenance Road Mileage" : parseFloat(o["Town Maintenance Road Mileage (per 000 population)"]),
+                "Road Mileage" : parseFloat(o["Town Maintenance Road Mileage (per 000 population)"]),
                 "Total Jobs" : parseFloat(o["Total Jobs (per capita)"])
             }
         });
@@ -76,12 +61,23 @@ $(document).ready(function(){
                 d3.range(0,5).map(function(n) { return "color_"+(n+1); })
             )
             .domain([
-                "Town Maintenance Road Mileage",
-                "Population Density",
-                "Total Jobs",
+                "Private-Sector Wage Index",
                 "Unemployment Rate",
-                "Private-Sector Wage Index"
+                "Total Jobs",
+                "Population Density",
+                "Road Mileage"
             ])
+
+        // tooltip function
+        var tip = d3.tip()
+            .attr("class", "barchart-tip")
+            .offset([-10, 0])
+            .direction("n")
+            .html(function(d) {
+                return colors.domain().map(function(indicator) {
+                    return indicator + ": " + decimalPercentFormat(d[indicator]);
+                }).join("<br />");
+            });
 
         function drawChart(container_width) {
             // console.log(container_width)
@@ -93,30 +89,33 @@ $(document).ready(function(){
             container.selectAll("svg").remove();
 
             // vars
-            var height = container_width * aspect_ratio;
             var width = container_width;
+            var height = d3.max([
+                container_width * aspect_ratio,
+                38 * 20 // capital region has 38 bars * 20 px each
+            ]);
 
             var margin = {
                 top: 0.09 * height,
-                right: 0.12 * width,
-                bottom: 0.1 * height,
-                left: 0.08 * width
+                right: 0.1 * width,
+                bottom: 0.05 * height,
+                left: 0.15 * width
             }
 
             // check bottom/left for minimums
             if (container_width < mobile_threshold) {
                 margin = {
-                    top: 0.15 * height,
-                    right: 0.15 * width,
-                    bottom: 0.28 * height,
-                    left: 0.16 * width
+                    top: 0.25 * height,
+                    right: 0.1 * width,
+                    bottom: 0.1 * height,
+                    left: 0.35 * width
                 }
             } else if (container_width < mobile_threshold * 1.5) {
                 margin = {
-                    top: 0.09 * height,
-                    right: 0.12 * width,
-                    bottom: 0.1 * height,
-                    left: 0.1 * width
+                    top: 0.2 * height,
+                    right: 0.11 * width,
+                    bottom: 0.08 * height,
+                    left: 0.25 * width
                 }
             }
 
@@ -134,80 +133,125 @@ $(document).ready(function(){
             width = (width-(margin.left + margin.right));
 
             // Scales
-            var x = d3.scale.ordinal()
-                .rangeRoundBands([0, width], 0.0);
+            var y = d3.scale.ordinal()
+                .rangeRoundBands([0, height], 0.0);
 
-            var y = d3.scale.linear()
-                .range([0, height*0.75])
+            var x = d3.scale.linear()
+                .range([0, width])
                 .domain([0, 100]);
 
             svgs.each(function(chartData, chartIndex) {
+                var svg = d3.select(this);
                 var cog = chartData.key;
                 var chartData = chartData.values;
 
-                var chart = d3.select(this).append("g")
-                    .classed("chart", true)
+                // could implement a sorting routine - click on legend entry to sort by that indicator
+                // var chartData = chartData.values.sort(function(a, b) {
+                //     return a["Total Jobs"] < b["Total Jobs"] ? 1 : -1;
+                // });
+
+                var title = svg.append("text")
+                    .classed("chart-title", true)
+                    .attr("text-anchor", "start")
+                    .attr("dy", "1em")
+                    .attr("font-size", "1.5em")
+                    .text(cog)
+
+
+                legendX = d3.scale.ordinal()
+                    .rangeRoundBands([0, width], 0.1);
+
+                legendY = d3.scale.ordinal()
+                    .rangeRoundBands([0, margin.top/2], 0.0);
+
+                // if (container_width < mobile_threshold) {
+                if (container_width < 1.5 * mobile_threshold) {
+                    legendX.domain([0]);
+                    legendY.domain(d3.range(0,5));
+                // } else if (container_width < 1.5 * mobile_threshold) {
+                //     legendX.domain(d3.range(0,2));
+                //     legendY.domain(d3.range(0,3));
+                } else {
+                    legendX.domain(d3.range(0,3));
+                    legendY.domain(d3.range(0,2));
+                }
+
+                var legend = svg.append("g")
+                    .classed("legend", true)
+                    .attr("transform", function() {
+                        if (container_width < 1.5 * mobile_threshold) {
+                            return "translate("+ margin.left +", " + (margin.top/3) + ")"
+                        } else {
+                            return "translate("+ margin.left +", " + (margin.top/2) + ")"
+                        }
+                    })
+                    .selectAll("g.legend-entry")
+                    .data(colors.domain())
+                    .enter()
+                        .append("g")
+                        .classed("legend-entry", true)
+                        .attr("transform", function(d, i) {
+                            var tx = legendX(i % legendX.domain().length);
+                            var ty = legendY(i % legendY.domain().length);
+                            return "translate(" + tx + ", " + ty + ")"
+                        })
+                        .each(function(entryData, i) {
+                            var entry = d3.select(this)
+
+                            entry.append("path")
+                                .attr("d", d3.svg.symbol().type("square").size(150))
+                                .attr("class", function() {
+                                    return colors(entryData);
+                                })
+                            entry.append("text")
+                                .attr("dx", 8)
+                                .attr("dy", 5)
+                                .text(entryData)
+                        })
+                var chart = svg.append("g")
+                    .attr("class", function(d) {
+                        return [
+                            "chart",
+                            "c_"+sluggify(cog)
+                        ].join(" ");
+                    })
                     .attr("height", height)
                     .attr("width", width)
-                    .classed(sluggify(cog), true)
                     .attr("transform", "translate("+ margin.left + ", " + margin.top + ")");
 
                 // update x scale domain
-                x.domain(
+                y.domain(
                     chartData.map(function(d) { return d["Municipality"]; })
                 )
 
                 // axes
+                var yAxis = d3.svg.axis()
+                    .scale(y)
+                    .orient("left");
+
+                var yAxis = chart.append("g")
+                    .attr("class", "axis yaxis")
+                    .call(yAxis)
+                    .call(function() {
+                        d3.selectAll("g.yaxis g.tick text")
+                            .attr("class", function() {
+                                return "m_" + sluggify(d3.select(this).text())
+                            })
+                    });
+
                 var xAxis = d3.svg.axis()
                     .scale(x)
-                    .orient("bottom");
+                    .orient("bottom")
+                    .tickFormat(integerPercentFormat);
+
+                if (container_width < (mobile_threshold * 1.5)) {
+                    xAxis.ticks(4);
+                }
 
                 var xAxis = chart.append("g")
                     .attr("class", "axis xaxis")
                     .attr("transform", "translate(0," + height + ")")
-                    .call(xAxis);
-
-                // if (container_width < mobile_threshold) {
-                  xAxis.call(function(g) {
-                      g.selectAll("g text")
-                          .attr("transform", "rotate(270)")
-                          .attr("text-anchor", "start")
-                          .attr("dy", function(d, i){
-                              if (i % 2 == 1) {
-                                  // ODD ticks
-                                  return "1.75em";
-                              } else {
-                                  // EVEN ticks
-                                  return "0.75em"
-                              }
-                          })
-                  })
-                // }
-
-                // var xAxisLabel = xAxis.append("text")
-                //     .attr("dy", "-0.5em")
-                //     .attr("dx", width)
-                //     // .attr("transform", "rotate(90)")
-                //     .style("text-anchor", "end")
-                //     .text("Municipality");
-                
-                var yAxis = d3.svg.axis()
-                    .scale(y)
-                    .orient("left")
-                    .tickFormat(integerPercentFormat);
-
-                if (container_width < (mobile_threshold * 1.5)) {
-                    yAxis.ticks(4);
-                }
-
-                var yAxis = chart.append("g")
-                    .attr("class", "axis yaxis")
-                    .call(yAxis);
-                
-                // var yAxisLabel = yAxis.append("text")
-                //     .attr("dy", "-0.25em")
-                //     .attr("dx", "0.5em")
-                //     .html("Gap/Net Gap");
+                    .call(xAxis)
                 
                 var barGroups = chart.selectAll("g.bar-group")
                     .data(chartData)
@@ -216,13 +260,20 @@ $(document).ready(function(){
                         .attr("class", function(d) {
                             return [
                                 "bar-group",
-                                sluggify(d["Municipality"]),
+                                "m_"+sluggify(d["Municipality"]),
+                                "c_"+sluggify(cog)
                             ].join(" ");
                         })
-                        .attr("transform", function(d) {
-                            return "translate(" + x(d["Municipality"]) + ", 0)";
+                        .attr("data-cog", function() {
+                            return "c_"+sluggify(cog);
                         })
-                        .call(townTip)
+                        .attr("data-municipality", function(d) {
+                            return "m_"+sluggify(d["Municipality"]);
+                        })
+                        .attr("transform", function(d) {
+                            return "translate(0 , " + y(d["Municipality"]) +")";
+                        })
+                        .call(tip)
                         .each(function(barData, barIndex) {
                             var bargroup = d3.select(this);
                             // make each of the five bars
@@ -232,107 +283,61 @@ $(document).ready(function(){
 
                                 bargroup
                                     .append("rect")
-                                        .attr("x", 0)
-                                        .attr("y", function() {
-                                            return y(offset)
+                                        .attr("x", function() {
+                                            return x(offset)
                                         })
-                                        .attr("height", function() {
-                                            return y(d);
+                                        .attr("y", 0)
+                                        .attr("width", function() {
+                                            return x(d);
                                         })
-                                        .attr("width", x.rangeBand())
+                                        .attr("height", y.rangeBand())
                                         .attr("class", function() {
                                             return [
                                                 "bar",
                                                 colors(indicator)
                                             ].join(" ");
                                         })
-                                        .call(indicatorTip)
 
                                     offset += d;
                             })
-                            // barData = .map(function(key, index, array) {
-                            //     if (index === 0) {
-                            //         offset = 0
-                            //     } else {
-                            //         offset = d3.sum(
-                            //             d3.range(0, index-1).map(function(i) {
-                            //                 return barData[array[index-1]];
-                            //             })
-                            //         )
-                            //     }
-                            //     return {key : key, values : barData[key], offset : offset};
-                            // })
+                            
+                            bargroup.append("rect")
+                                .classed("hover-target", true)
+                                .attr("x", 0)
+                                .attr("y", 0)
+                                .attr("width", width)
+                                .attr("height", y.rangeBand())
                         })
 
                 return;
             })
 
             // register hover events (tooltips) for bar groups
-            // d3.selectAll("g.bar-group")
-            //     .on("mouseover", function(d) {
-            //         townTip.show(d)
-            //     })
-            //     .on("mouseout", function(d) {
-            //         townTip.hide(d)
-            //     })
-            // d3.selectAll("rect.bar")
-            //     .on("mouseover", function(d) {
-            //         indicatorTip.show(d)
-            //     })
-            //     .on("mouseout", function(d) {
-            //         indicatorTip.hide(d)
-            //     })
+            d3.selectAll("g.bar-group")
+                .on("mouseover", function(d) {
+                    var highlightCog = d3.select(this).attr("data-cog");
+                    var highlightMunicipality = d3.select(this).attr("data-municipality");
 
-            return;
-            d3.selectAll("rect.hover-target")
-                .on("mouseover", function(d){
-                    var highlightQuintile = d3.select(this).attr("data-quintile");
+                    var toHighlight = d3.selectAll("g.chart."+highlightCog+" g.bar-group."+highlightMunicipality+", g.chart."+highlightCog+" text."+highlightMunicipality);
+                    var toLowlight = d3.selectAll("g.chart."+highlightCog+" g.bar-group:not(."+highlightMunicipality+"), g.chart."+highlightCog+" text:not(."+highlightMunicipality+")");
 
-                    var toHighlight = d3.selectAll("g.bar-group."+highlightQuintile)
-                    var toLowlight = d3.selectAll("g.bar-group:not(."+highlightQuintile+")")
 
-                    toLowlight.classed({
-                            "lowlight": true
-                        });
+                    console.log("g.chart."+highlightCog+" g.bar-group."+highlightMunicipality+", g.chart."+highlightCog+" text."+highlightMunicipality)
+
+                    toHighlight
+                        .classed("highlight", true);
+                    toLowlight
+                        .classed("lowlight", true);
                     
-                    toHighlight.classed({
-                            "highlight": true
-                        });
-
-                    tip.show(d);
+                    tip.show(d)
                 })
-                .on("mouseout", function(d){
+                .on("mouseout", function(d) {
                     d3.selectAll(".highlight, .lowlight")
                         .classed({
                             "lowlight": false,
                             "highlight": false
                         });
-
-                    tip.hide(d);
-                });
-
-            d3.selectAll("div.legend-entry")
-                .on("mouseover", function(){
-                    var highlightClass = d3.select(this).attr("data-type");
-
-                    var toHighlight = d3.selectAll("div.legend-entry."+highlightClass+", rect.bar."+highlightClass)
-                    var toLowlight = d3.selectAll("div.legend-entry:not(."+highlightClass+"), rect.bar:not(."+highlightClass+")")
-
-                    toLowlight.classed({
-                            "lowlight": true
-                        });
-
-                    
-                    toHighlight.classed({
-                            "highlight": true
-                        });
-                })
-                .on("mouseout", function(){
-                    d3.selectAll(".highlight, .lowlight")
-                        .classed({
-                            "highlight": false,
-                            "lowlight": false
-                        });
+                    tip.hide(d)
                 })
         }
 
